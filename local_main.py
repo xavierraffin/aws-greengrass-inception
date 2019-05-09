d1ea37d2ec22e64f205a990f5d9fe81f42f3a82e
Xavier Raffin <xraffin@amazon.com>
2019-05-09 14:25:09 -0700

    added missing synset.txt

--- local_main.py
+++ local_main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import load_model
 
-exists=(os.path.isfile("Inception-BN-symbol.json") and os.path.isfile("Inception-BN-0000.params"))
+exists=(os.path.isfile("Inception-BN-symbol.json") and os.path.isfile("Inception-BN-0000.params") and os.path.isfile("synset.txt"))
 
 def predict_cam():
     model_path = './'
@@ -15,6 +15,7 @@ if not exists:
     import urllib
     urllib.urlretrieve ("http://data.mxnet.io/models/imagenet/inception-bn/Inception-BN-symbol.json", "Inception-BN-symbol.json")
     urllib.urlretrieve ("http://data.mxnet.io/models/imagenet/inception-bn/Inception-BN-0126.params", "Inception-BN-0000.params")
+    urllib.urlretrieve ("http://data.mxnet.io/mxnet/models/imagenet/synset.txt", "synset.txt")
     print("Models downloaded")
 
 predict_cam()
